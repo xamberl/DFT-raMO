6de5ac014865ed9be00126d211e4bc2d205956fe
xamberl <xalim@wisc.edu>
2022-08-12 15:25:03 -0500

    Add script for reading VASP outputs

---
+++ src/readVASP.jl
@@ -0,0 +1,19 @@
+# Initial script to read VASP inputs
+
+# Transform WAVECAR to data [TBD!!!]
+
+# Grab fermi energy from OUTCAR
+function getFermi(io::IO)
+    readuntil(io, "E-fermi :")
+    fermi = parse.(Float64, split(readline(io))[1])
+    return fermi
+end
+
+# Grab supercell dimensions from KPOINTS
+function getSupercell(io::IO)
+    supercelldims = parse.(Int64,split(readlines(io)[4]))
+    return supercelldims
+end
+
+export getFermi, getSupercell
+
