4ff99857b275a5704a8897e6f5478f42c29c47d6
Amber Lim <xalim@wisc.edu>
2022-10-10 13:52:34 -0500

    Added Supercell struct & make_target_AO function

--- src/raMO.jl
+++ src/raMO.jl
@@ -16,3 +16,15 @@ function make_overlap_mat(occ_coeff, kpoint_repeating::Vector{Bool})
     S[kpoint_zero,kpoint_zero] = occ_coeff[:,kpoint_zero]'*occ_coeff[:,kpoint_zero]
     return S
 end
+
+"""
+    make_target_AO(make_target_AO(atom_site::Int, orbital_to_use::Int, total_num_orbitals::Int) -> Vector
+
+Returns a vector of length total_num_orbitals with "1" in the corresponding atomic orbital
+"""
+function make_target_AO(atom_site::Int, target_orbital::Int, super::Supercell)
+    # psi_target has a length of total number of orbitals in the supercell
+    psi_target = zeros(sum(super.orbitals))
+    psi_target[sum(super.orbitals[1:atom_site])-super.orbitals[atom_site]+target_orbital] = 1
+    return psi_target
+end
