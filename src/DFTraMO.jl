ad715fef2fe1bebd799fee080a2aa8edc392a0e2
Amber Lim <xalim@wisc.edu>
2022-10-11 17:02:34 -0500

    Clarified some comments on functions

--- src/DFTraMO.jl
+++ src/DFTraMO.jl
@@ -12,7 +12,8 @@ include("data.jl")
 export OrbitalParams, ehtParams, Supercell
 
 include("raMO.jl")
-export get_eht_params, make_overlap_mat, make_target_AO, make_target_cluster_sp, generate_H, reconstruct_targets_DFT
+export get_eht_params, make_overlap_mat, make_target_AO, make_target_cluster_sp, make_target_hybrid,
+generate_H, reconstruct_targets_DFT
 
 include("inputs.jl")
 export create_run, extract_VASP, read_GCOEFF, track_kpoint_repeating, read_eht_params, read_site_list
--- src/inputs.jl
+++ src/inputs.jl
@@ -11,6 +11,9 @@ function create_run(run_name::AbstractString)
 end
 
 """
+    read_run_in(run_name::AbstractString)
+
+Reads and loads options for the run specified by a file.
 """
 function read_run_in(run_name::AbstractString)
     # initialize outputs
@@ -55,95 +58,97 @@ function read_run_in(run_name::AbstractString)
 end
 
 """
-extract_VASP
+extract_VASP(d::AbstractString="") -> fermi::Float64, geo::Crystal{3}, kpt::KPointGrid{3}, super::AtomList{3}, wave::ReciprocalWaveFunction{3,Float32}
 
-Searches in the current directory for VASP files and extracts relevant information.
+Searches in the specified directory (default is the current directory) for VASP files
+OUTCAR, POSCAR, KPOINTS, and WAVECAR, and extracts relevant information.
 """
-function extract_VASP()
-    fermi = get_fermi("OUTCAR")    
-    geo = readPOSCAR("POSCAR")
-    kpt = readKPOINTS("KPOINTS")
-    wave = readWAVECAR("WAVECAR")
+function extract_VASP(d::AbstractString="")
+    fermi = get_fermi(String(d,"OUTCAR"))
+    geo = readPOSCAR(String(d,"POSCAR"))
+    kpt = readKPOINTS(String(d,"KPOINTS"))
+    wave = readWAVECAR(String(d,"WAVECAR"))
     # Creates an AtomList{3} supercell from POSCAR and KPOINTS
     super = supercell(geo.atoms,kpt.grid)
     return fermi, geo, kpt, super, wave
 end
 
-    function read_GCOEFF(emin::Real, emax::Real)
-        open("GCOEFF.txt","r") do io
-            num_spin_states = parse(Int,readline(io))
-            num_kpt = parse(Int,readline(io))
-            num_band = parse(Int,readline(io))
-            # skips the next 6 lines, which correspond to a, b, c, a*, b*, c*
-            for i in 1:6
-                readline(io);
-            end
-            # Regarding spin states
-            if num_spin_states == 2
-                num_spin_up = 0
-                num_spin_down = 0
-            end
-            # Initialize containers
-            num_occ_states = 0
-            kptlist = Array{Vector{Float64}}(undef,0)
-            npw_temp = 0
-            current_G = Array{Vector{Int}}(undef,0)
-            current_occ_coeff = []
-            # Loop through spin states, kpts, band
-            for s in 1:num_spin_states
-                println("Reading spin state ", s)
-                for k in 1:num_kpt
-                    current_kpt = parse.(Float64,split(readline(io)))
-                    # Counter for occupied states in kpt
-                    occ_states_k = 0
-                    for b in 1:num_band
-                        current_band,num_pw = parse.(Int,split(readline(io)))
-                        # Read in Energy, Filling
-                        ln = split(readline(io))
-                        # Need if statement somewhere to check if we've reached the end of the bands
-                        energy, filling = parse.(Float64, [ln[2],ln[6]])
-                        # Reads in info for only occupied states
-                        if energy >= emin && energy <= emax
-                            # Counts spin up/down states
-                            if num_spin_states == 2
-                                if s == 1
-                                    num_spin_up += 1
-                                else
-                                    num_spin_down += 1
-                                end
-                            end
-                            # Counts occupied states, updates kptlist
-                            occ_states_k = occ_states_k + 1
-                            num_occ_states = num_occ_states + 1
-                            push!(kptlist,current_kpt)
-                            #temp_occ_coeff = Vector{Complex}(undef,0)
-                            for p in 1:num_pw
-                                # Counter to keep track of which G we're at
-                                g_count = 1
-                                ln = split(readline(io))
-                                # If the G value is unique, append it to G, and temp_occ_coeff
-                                if !(parse.(Int,ln[1:3]) in current_G)
-                                    npw_temp = npw_temp+1
-                                    push!(current_G, parse.(Int,ln[1:3]))
-                                    push!(current_occ_coeff, complex(parse(Float64,ln[5]),parse(Float64,ln[7])))
-                                # Otherwise, put it in the corresponding slot
-                                else
-                                    push!(current_occ_coeff, complex(parse(Float64,ln[5]),parse(Float64,ln[7])))
-                                end
-                            end
-                        else
-                            for p in 1:num_pw
-                                readline(io)
+# read_GCOEFF() is a WIP
+function read_GCOEFF(emin::Real, emax::Real)
+    open("GCOEFF.txt","r") do io
+        num_spin_states = parse(Int,readline(io))
+        num_kpt = parse(Int,readline(io))
+        num_band = parse(Int,readline(io))
+        # skips the next 6 lines, which correspond to a, b, c, a*, b*, c*
+        for i in 1:6
+            readline(io);
+        end
+        # Regarding spin states
+        if num_spin_states == 2
+            num_spin_up = 0
+            num_spin_down = 0
+        end
+        # Initialize containers
+        num_occ_states = 0
+        kptlist = Array{Vector{Float64}}(undef,0)
+        npw_temp = 0
+        current_G = Array{Vector{Int}}(undef,0)
+        current_occ_coeff = []
+        # Loop through spin states, kpts, band
+        for s in 1:num_spin_states
+            println("Reading spin state ", s)
+            for k in 1:num_kpt
+                current_kpt = parse.(Float64,split(readline(io)))
+                # Counter for occupied states in kpt
+                occ_states_k = 0
+                for b in 1:num_band
+                    current_band,num_pw = parse.(Int,split(readline(io)))
+                    # Read in Energy, Filling
+                    ln = split(readline(io))
+                    # Need if statement somewhere to check if we've reached the end of the bands
+                    energy, filling = parse.(Float64, [ln[2],ln[6]])
+                    # Reads in info for only occupied states
+                    if energy >= emin && energy <= emax
+                        # Counts spin up/down states
+                        if num_spin_states == 2
+                            if s == 1
+                                num_spin_up += 1
+                            else
+                                num_spin_down += 1
                             end
                         end
+                        # Counts occupied states, updates kptlist
+                        occ_states_k = occ_states_k + 1
+                        num_occ_states = num_occ_states + 1
+                        push!(kptlist,current_kpt)
+                        #temp_occ_coeff = Vector{Complex}(undef,0)
+                        for p in 1:num_pw
+                            # Counter to keep track of which G we're at
+                            g_count = 1
+                            ln = split(readline(io))
+                            # If the G value is unique, append it to G, and temp_occ_coeff
+                            if !(parse.(Int,ln[1:3]) in current_G)
+                                npw_temp = npw_temp+1
+                                push!(current_G, parse.(Int,ln[1:3]))
+                                push!(current_occ_coeff, complex(parse(Float64,ln[5]),parse(Float64,ln[7])))
+                                # Otherwise, put it in the corresponding slot
+                            else
+                                push!(current_occ_coeff, complex(parse(Float64,ln[5]),parse(Float64,ln[7])))
+                            end
+                        end
+                    else
+                        for p in 1:num_pw
+                            readline(io)
+                        end
                     end
-                    println("For K-point # ", k, ", Spin State ", s, ", found ", occ_states_k, " Occupied States.")
                 end
+                println("For K-point # ", k, ", Spin State ", s, ", found ", occ_states_k, " Occupied States.")
             end
-            println("Total occupied states: ", num_occ_states," npw_temp: ", npw_temp)
-            return current_G, current_occ_coeff, kptlist
         end
+        println("Total occupied states: ", num_occ_states," npw_temp: ", npw_temp)
+        return current_G, current_occ_coeff, kptlist
     end
+end
 
 # Repeated kpoints marked with 0; unrepeated marked with 1.
 function track_kpoint_repeating(kptlist::Vector{Vector{Float64}})
@@ -158,7 +163,7 @@ function track_kpoint_repeating(kptlist::Vector{Vector{Float64}})
 end
 
 """
-    read_eht_params(paramsfile::AbstractString) -> ehtParams 
+    read_eht_params(paramsfile::AbstractString) -> mat::ehtParams 
 
 Reads an eHtuner parameter file and returns it as an ehtParams object.
 If argument is left empty, it will read the DFT_raMO_eht_parms.dat file by default.
