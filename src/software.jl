ef4ee5854ceb0d6b9cf132d2620022caf9bea82f
Brandon Flores <bsflores@wisc.edu>
2023-07-14 15:36:33 -0500

    Created new `raMOinput` struct

---
+++ src/software.jl
@@ -0,0 +1,59 @@
+"""
+    raMOInput
+
+Contains all of the crystal and wavefunction information needed to perform a DFT-raMO run.
+"""
+struct raMOInput
+    xtal::Crystal
+    wave::PlanewaveWavefunction
+    fermi::Float64
+end
+
+Electrum.Crystal(x::raMOInput) = x.xtal
+Electrum.PeriodicAtomList(x::raMOInput) = x.xtal.atoms
+Electrum.PlanewaveWavefunction(x::raMOInput) = x.wave
+Electrum.fermi(x::raMOInput) = x.fermi
+kptmesh(x::raMOInput) = x.xtal.set_transform
+
+abstract type InputOrigin
+end
+
+struct FromABINIT
+end
+
+struct FromVASP
+end
+
+function raMOInput(io::IO, ::FromABINIT)
+    h = Electrum.read_abinit_header(io)
+    seekstart(io)
+    return raMOinput(Crystal(h), read_abinit_WFK(io)["wavefunction"], h[:fermi])
+end
+
+raMOInput(file, ::FromABINIT) = open(io -> raMOInput(io, FromABINIT()), file)
+
+function raMOInput(
+    ::FromVASP;
+    POSCAR = "POSCAR",
+    WAVECAR = "WAVECAR",
+    KPOINTS = "KPOINTS",
+    OUTCAR = "OUTCAR"
+)
+    fermi = get_fermi(OUTCAR)
+    geo = readPOSCAR(POSCAR)
+    wave = readWAVECAR(WAVECAR, quiet = true)
+    kpt = parse.(Int, split(readlines(KPOINTS)[4]))
+    # Use a Crystal to lazily reference the supercell
+    xtal = set_transform!(Crystal(geo), kpt)
+    return raMOInput(xtal, wave, fermi) 
+end
+
+function raMOInput(directory::AbstractString, ::FromVASP)
+    return raMOInput(
+        FromVASP();
+        POSCAR = joinpath(directory, POSCAR),
+        WAVECAR = joinpath(directory, WAVECAR),
+        KPOINTS = joinpath(directory, KPOINTS),
+        OUTCAR = joinpath(directory, OUTCAR),
+    )
+end
