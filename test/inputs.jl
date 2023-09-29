628685a4c4894e9e0c0a9c60b194b664347821b4
Amber Lim <xalim@wisc.edu>
2023-09-29 15:42:18 -0500

    Tests for `parse_sites`

--- test/inputs.jl
+++ test/inputs.jl
@@ -2,4 +2,7 @@
     cd("input/") do
         input = read_run_yaml("ScAl3.yaml")
     end
+
+    @test DFTraMO.parse_sites(["1:10"]) == collect(1:10)
+    @test DFTraMO.parse_sites(["10", "10", "10:2:16"]) == [10, 12, 14, 16]
 end
