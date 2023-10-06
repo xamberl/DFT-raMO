c7b3f17e6382880b51d028ebe1da558140ddda4e
Amber Lim <xalim@wisc.edu>
2023-10-06 14:17:42 -0500

    Added tests for emin/emax

--- test/inputs.jl
+++ test/inputs.jl
@@ -1,5 +1,8 @@
 @testset "Inputs" begin
     cd("input/") do
-        input = read_run_yaml("ScAl3.yaml")
+        (runs, checkpoint, auto_psphere, dftinfo, emin, emax) = read_run_yaml("ScAl3.yaml")
+        @test length(runs) == 5
+        @test emin == -100*DFTraMO.Electrum.EV2HARTREE
+        @test emax == 0.25
     end
 end
