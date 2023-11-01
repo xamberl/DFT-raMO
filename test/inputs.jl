aa04f7b942969ea09da4f687c09d2198f2bec5bc
Amber Lim <xalim@wisc.edu>
2023-11-01 13:53:17 -0500

    Test `read_yaml`

--- test/inputs.jl
+++ test/inputs.jl
@@ -1,9 +1,9 @@
 @testset "Inputs" begin
     cd("input/") do
-        (runs, checkpoint, auto_psphere, dftinfo, emin, emax) = read_run_yaml("ScAl3.yaml")
-        @test length(runs) == 5
-        @test emin == -100*DFTraMO.Electrum.EV2HARTREE
-        @test emax == 0.25
+        ramoinput = read_yaml("ScAl3.yaml")
+        @test length(ramoinput.runlist) == 5
+        @test ramoinput.emin == -100*DFTraMO.Electrum.EV2HARTREE
+        @test ramoinput.emax == 0.25
     end
 
     @test DFTraMO.parse_sites(["10", "10", "10:2:16"]) == [10, 12, 14, 16]
