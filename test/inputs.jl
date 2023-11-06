9a22ba89b22f5950de3253a6c527f88e00d1683a
Amber Lim <xalim@wisc.edu>
2023-11-06 13:10:17 -0600

    Rearrange tests

--- test/inputs.jl
+++ test/inputs.jl
@@ -1,13 +1,12 @@
 @testset "Inputs" begin
+    @test DFTraMO.parse_sites(["10", "10", "10:2:16"]) == [10, 12, 14, 16]
+    
     cd("input/") do
         ramoinput = read_yaml("ScAl3.yaml")
         @test length(ramoinput.runlist) == 5
         @test ramoinput.emin == -100*DFTraMO.Electrum.EV2HARTREE
         @test ramoinput.emax == 0.25
+        # Test a full run
+        @test DFTraMO.dftramo_run("ScAl3.yaml")
     end
-
-    @test DFTraMO.parse_sites(["10", "10", "10:2:16"]) == [10, 12, 14, 16]
-
-    # Test a full run
-    @test DFTraMO.dftramo_run("ScAl3.yaml")
 end
