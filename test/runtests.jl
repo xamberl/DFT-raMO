ead9f81ada2ce7e1770a48aa9c814c7dc45eceb4
Brandon Flores <bsflores@wisc.edu>
2023-11-07 16:07:24 -0600

    Added test set for `OccupiedStates`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using Test
+using Electrum
 using DFTraMO
 
 @testset "All tests" begin
     # Add your tests here, ideally as file includes.
     include("inputs.jl")
+    include("occupiedstates.jl")
 end
