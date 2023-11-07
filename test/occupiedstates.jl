f586d69239c897b7fe4a4aa4a75d5fb9bd14affa
Brandon Flores <bsflores@wisc.edu>
2023-11-07 16:48:34 -0600

    Fixed missing quotes for `@testset`

--- test/occupiedstates.jl
+++ test/occupiedstates.jl
@@ -1,4 +1,4 @@
-@testset OccupiedStates begin
+@testset "OccupiedStates" begin
     wf = readWAVECAR("input/WAVECAR")
     # Fermi energy is the maximum...obtain it separately
     emax = get_fermi("input/OUTCAR")[:fermi]
