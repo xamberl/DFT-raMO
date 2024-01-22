f19f60cf3274f0b42099865e3d56f5ec817005e9
Amber Lim <xalim@wisc.edu>
2024-01-22 17:28:33 -0600

    Update test for parsing radii

--- test/inputs.jl
+++ test/inputs.jl
@@ -5,6 +5,7 @@
         @test ramoinput.emin == -100*DFTraMO.Electrum.EV2HARTREE
         @test ramoinput.emax == 0.25
         @test ramoinput.mode == "auto_psphere"
+        @test ramoinput.runlist[5].radius == 3.97*DFTraMO.Electrum.BOHR2ANG
     end
 
     @test DFTraMO.parse_sites(["10", "10", "10:2:16"]) == [10, 12, 14, 16]
