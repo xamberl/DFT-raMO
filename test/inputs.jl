c685461771eb45709705af8f0b02b3ba7c627b02
Amber Lim <xalim@wisc.edu>
2024-02-06 15:15:15 -0600

    Add tests for reading displaced AOs input

--- test/inputs.jl
+++ test/inputs.jl
@@ -6,6 +6,13 @@
         @test ramoinput.emax == 0.25
         @test ramoinput.mode == "auto_psphere"
         @test ramoinput.runlist[5].radius == 3.97*DFTraMO.Electrum.BOHR2ANG
+
+        ramoinput = read_yaml("displaced_aos.yml")
+        @test ramoinput.mode == "discard"
+        @test ramoinput[1].sites == [1, 3, 7, 8]
+        @test ramoinput[1].type == "dx2y2"
+        @test ramoinput[1].direction == [0.5, 0.5, 0]/DFTraMO.norm([0.5, 0.5, 0])
+        @test ramoinput[1].radius == 0.5
     end
 
     @test DFTraMO.parse_sites(["10", "10", "10:2:16"]) == [10, 12, 14, 16]
