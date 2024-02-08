66f625b77ae6d6bf0ace7d91894f9ba6bd4bb097
Amber Lim <xalim@wisc.edu>
2024-02-08 11:50:18 -0600

    Enable tests for multiple yamls

--- test/dftramo_run.jl
+++ test/dftramo_run.jl
@@ -2,5 +2,8 @@
     cd("input/") do
         @test dftramo_run("ScAl3_short.yaml") == nothing
         rm("1_Sc-Sc", force=true, recursive=true)
+
+        @test dftramo_run("displaced_aos.yml") == nothing
+        rm("Sc_custom_dx2y2", force=true, recursive=true)
     end
 end
